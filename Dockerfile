2e9bab4100f50c7d83e82bb07a1928991969c719
Kevin Whittington <kevin.whittington@ngc.com>
2023-08-09 16:34:44 -0400

    Fixed spring

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM openjdk:8
+FROM adoptopenjdk/openjdk11
 ADD target/s2-movie-info-service-0.0.1-SNAPSHOT.jar s2-movie-info-service-0.0.1-SNAPSHOT.jar
 EXPOSE 8082
 ENTRYPOINT ["java", "-jar", "s2-movie-info-service-0.0.1-SNAPSHOT.jar"]
--- run_build.sh
+++ run_build.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
+
+int_handler() {
+    echo "Interrupted..."
+    # Kill the parent process of this script.
+    kill $PPID
+    exit 1
+}
+
+trap 'int_handler' INT
+
 echo "Start"
+
 BUILD_NUMBER="v1"
 DOCKER_HUB="docker-ng-staging.northgrum.com"
-IMAGE_NAME="kevinbwusa/s2-movie-info-service:${BUILD_NUMBER}"
+IMAGE_NAME="${DOCKER_HUB}/kevinbwusa/s2-movie-info-service:${BUILD_NUMBER}"
+
 echo "Creating Image..."
 echo "IMAGE_NAME--- $IMAGE_NAME"
-sudo docker build -t $IMAGE_NAME .
-sudo docker login -u kevinbwusa -p${DOCKER_HUB}
-sudo docker push $IMAGE_NAME
+docker build -t $IMAGE_NAME .
+# sudo docker login -u kevinbwusa -p${DOCKER_HUB}
+docker push $IMAGE_NAME
 echo "Deploying Image to Kubernetes..."
 export KUBECONFIG=/home/ubuntu/kubeconfig/kubeConfig/kube-config-mil01-rana-cluster.yml
 kubectl set image deployment.v1beta1.extensions/s2-movie-info-service-deployment s2-movie-info-service=$IMAGE_NAME
